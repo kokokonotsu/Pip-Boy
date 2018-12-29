9b189dc4d5c29ed584243916b4842cc79de32dce
Ethan Simpson <ethansown@gmail.com>
2018-12-29 10:50:03 -0600

    Changed the amount scrolled for inventory

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 var inventory = document.getElementsByClassName("inventory-list-container")[0];
 var inventory_scroll_up_button = document.getElementsByClassName("inventory-before")[0];
 var inventory_scroll_down_button = document.getElementsByClassName("inventory-after")[0];
+var inventoryHeight = inventory.offsetHeight;
+var offsetChangeUp = ((inventoryHeight/100) * 10) * -1;
+var offsetChangeDown = (inventoryHeight/100) * 10;
 var lastKnownScrollPosition = 0;
 var ticking;
-
-function scrollItUp(){
-    inventory.scrollBy(0, -50);
-    console.log(inventory.scrollTop);
+console.log(offsetChangeUp);
+function scrollItUp(){    
+    inventory.scrollBy(0, offsetChangeUp);
 }
 function scrollItDown(){
-    inventory.scrollBy(0, 50);
-    console.log(inventory.scrollTop);
+    inventory.scrollBy(0, offsetChangeDown);
 }
 inventory_scroll_up_button.addEventListener("click", (e) => {
     lastKnownScrollPosition = inventory.scrollTop;
