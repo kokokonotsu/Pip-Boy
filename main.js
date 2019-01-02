d3c5d2d09f40945b26a4478f764d69486f128811
Ethan Simpson <ethansown@gmail.com>
2019-01-02 12:39:55 -0600

    Various Changes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var inventory = document.getElementsByClassName("inventory-list-container")[0];
+var inventoryItem = document.getElementsByClassName("inventory-item-container");
 var inventory_scroll_up_button = document.getElementsByClassName("inventory-before")[0];
 var inventory_scroll_down_button = document.getElementsByClassName("inventory-after")[0];
 var inventoryHeight = inventory.offsetHeight;
@@ -8,9 +9,15 @@ var inventoryItemCheckBoxes = document.getElementsByClassName("inventory-item-ch
 var lastKnownScrollPosition = 0;
 var ticking;
 console.log(offsetChangeUp);
+//Add Attributes
 for(let i = 0; i < inventoryItemCheckBoxes.length; i++){
     inventoryItemCheckBoxes[i].setAttribute("onclick", "check(event)");
 }
+for(let i = 0; i < inventoryItem.length; i++){
+    inventoryItem[i].setAttribute("onmouseover", "itemHover(event)");
+    inventoryItem[i].setAttribute("onmouseleave", "itemOut(event)");
+}
+//CheckBox Function
 function check(e){
     if(!e.target.classList.contains("checked")){
         e.target.classList.add("checked");
@@ -22,6 +29,22 @@ function check(e){
     }
     else if(e.target.classList.contains("checked")){
         e.target.classList.remove("checked");
+    }
+}
+//Item Hover Function
+function itemHover(e){
+    if(e.target.firstChild.className == "inventory-item-checkbox"){ 
+        e.target.firstChild.classList.add("highlight");
+    } else if(e.target.className == "inventory-item-checkbox"){
+        e.target.classList.add("highlight");
+    }
+}
+//Item Out Function
+function itemOut(e){
+    if(e.target.firstChild.className == "inventory-item-checkbox" || e.target.className == "inventory-item-checkbox"){
+        e.target.firstChild.classList.remove("highlight");
+        console.log("I am running");
+
     }
 }
 //Scrolling via JS
